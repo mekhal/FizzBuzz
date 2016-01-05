6917d6bcfbef7f7ad4cca9350ce2119669443c0b
mekhal <mekha.l@outlook.com>
2016-01-05 11:54:39 +0700

    Remove Duplicate Code

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,23 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzbuzz = new FizzBuzz();
+
     @Test
     public void put_1_return_1(){
-        String expected = "1";
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        String actual = fizzbuzz.say(1);
-
-        assertEquals(expected,actual);
+        assertEquals("1",fizzbuzz.say(1));
     }
 
     @Test
     public  void put_2_return_2(){
-        String expected = "2";
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        String actual = fizzbuzz.say(2);
-
-        assertEquals(expected,actual);
+        assertEquals("2",fizzbuzz.say(2));
     }
 }
