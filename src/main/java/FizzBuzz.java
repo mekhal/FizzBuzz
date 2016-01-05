1c3ce6c4db376bd1104a063ccd286f32363cf2df
mekhal <mekha.l@outlook.com>
2016-01-05 11:36:41 +0700

    Rename variable i to number

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,6 @@
-
 public class FizzBuzz {
 
-    public String say(int i) {
+    public String say(int number) {
         return "1";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,14 +5,11 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void put_1_return_1(){
-        //Arrange
         String expected = "1";
         FizzBuzz fizzbuzz = new FizzBuzz();
 
-        //act
         String actual = fizzbuzz.say(1);
 
-        //assert
         assertEquals(expected,actual);
     }
 }
