e1fbf8d999889a4b2da98fe43f243d48e90c119b
mekhal <mekha.l@outlook.com>
2016-01-05 11:45:39 +0700

    Add Put_2_Return_2

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
 
     public String say(int number) {
-        return "1";
+        return Integer.toString(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,12 +3,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
     @Test
     public void put_1_return_1(){
         String expected = "1";
         FizzBuzz fizzbuzz = new FizzBuzz();
 
         String actual = fizzbuzz.say(1);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public  void put_2_return_2(){
+        String expected = "2";
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        String actual = fizzbuzz.say(2);
 
         assertEquals(expected,actual);
     }
